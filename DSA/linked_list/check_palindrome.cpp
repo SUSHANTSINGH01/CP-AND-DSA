69576884958764d811efb62020e120be64c0f765
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-30 12:41:51 +0530

    Create check_palindrome.cpp

---
+++ DSA/linked_list/check_palindrome.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+ // } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        //Your code here
+        if(head==0)
+        return 0;
+       
+       
+       if(head->next==0)
+       return 1;
+           
+       string s="";
+       while(head!=0)
+       {
+           s+=to_string(head->data);
+           head=head->next;
+       }
+       
+       for(int i=0;i<s.size();i++)
+       {
+           if(s[i]!=s[s.size()-i-1])
+           return 0;
+       }
+       
+       return 1;
+   }
+};
+
+
+
+// { Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
