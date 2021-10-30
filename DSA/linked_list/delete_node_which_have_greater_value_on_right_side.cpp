5be21557a130ee7e98ae45e5e595b62bedb6fffe
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-30 20:27:07 +0530

    Create delete_node_which_have_greater_value_on_right_side.cpp

---
+++ DSA/linked_list/delete_node_which_have_greater_value_on_right_side.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        Node* curr=head;
+        Node* temp=0;
+        Node* prev=0;
+        
+       
+        while(curr)
+        {
+            temp=curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=temp;
+        }
+         
+        
+        curr=prev;
+        prev=0;
+        
+        while(curr)
+        {
+            temp=curr->next;
+          //  cout<<curr->next->data<<" ";
+            while(temp && temp->data < curr->data)
+            {
+                temp = temp->next;
+            }
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
+   
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
