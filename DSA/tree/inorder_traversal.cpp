27d103f15cf90157b1b6085b232eda0b3daa914c
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-31 12:06:08 +0530

    Create inorder_traversal.cpp

---
+++ DSA/tree/inorder_traversal.cpp
@@ -0,0 +1,68 @@
+// recusive
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        if(root==NULL)
+        return ans;
+        
+        inorderTraversal(root->left);
+        ans.push_back(root->val);
+        inorderTraversal(root->right);
+        return ans;
+    }
+};
+
+
+
+// Iterative
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        stack<TreeNode*> st;
+        vector<int>ans;
+        if(root==0) 
+        return ans;
+        auto node=root;
+        
+        while(node || !st.empty())
+        {
+            while(node)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            node=st.top();
+            ans.push_back(node->val);
+            st.pop();
+            node=node->right;
+        }
+        return ans; 
+    }
+};
