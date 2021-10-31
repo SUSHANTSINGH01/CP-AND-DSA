6bdad6c9d17593e4f907502c3c6346ff9c4d8b29
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-31 12:26:24 +0530

    Create postorder_traversal.cpp

---
+++ DSA/tree/postorder_traversal.cpp
@@ -0,0 +1,81 @@
+// recursive
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        if(root==NULL)
+        return ans;
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        ans.push_back(root->val);
+        return ans;
+        
+    }
+};
+
+
+// iterative
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        vector<int>ans;
+        if(!root) 
+        return ans;
+        stack<TreeNode*>st;
+        auto node = root;
+        while(node || !st.empty())
+        {
+            while(node)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            auto temp = st.top()->right;
+            if(temp) 
+            node = temp;
+            
+            
+            else
+            {
+                temp = st.top();
+                 st.pop();
+                ans.push_back(temp->val);
+               
+                while(!st.empty() && temp == st.top()->right)
+                {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                }
+            }
+        }
+        return ans;
+    }
+};
