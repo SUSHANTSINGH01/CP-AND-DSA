4b463b53798c0350983e0eeb1ec287cbcd4049cd
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-10-31 11:27:46 +0530

    Create level_order_traversal.cpp

---
+++ DSA/linked_list/level_order_traversal.cpp
@@ -0,0 +1,38 @@
+vector<vector<int>>ans1;
+    vector<vector<int>> levelOrderBottom(TreeNode* root) 
+    {
+      vector<vector<int>> ans;
+		
+        if(root==NULL)
+        return ans;
+		
+        queue<TreeNode*> qu;
+		
+        qu.push(root);
+       
+        while(!qu.empty()) 
+        {
+            int n=qu.size(); 
+            vector<int> arr;
+            while(n>0) 
+            {
+                TreeNode* node = qu.front();
+                qu.pop();
+                arr.push_back(node->val);
+                if(node->left)
+                    qu.push(node->left);
+                if(node->right)
+                    qu.push(node->right);
+                n--;
+            }
+            ans.push_back(arr);
+        }
+        for(int i=ans.size()-1;i>=0;i--)
+        {
+            vector<int>arr1;
+            for(int j=0;j<ans[i].size();j++)
+            arr1.push_back(ans[i][j]);
+            ans1.push_back(arr1);
+        }
+        return ans1;   
+    }
