a887df8c683004558eb6114e4253e37d0e353082
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-01 01:58:21 +0530

    Create cc_largest_laddu.cpp

---
+++ CP/easy/cc_largest_laddu.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define md 1000000007
+#define fast ios_base::sync_with_stdio(false)
+#define fast1 cin.tie(0)
+#define fast2 cout.tie(0)
+#define ulli long long int 
+int main()
+{
+    fast;
+    fast1;
+    fast2;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n; 
+        cin>>n;
+        int m=pow(2,n);
+        vector<int>arr(m);
+        for(int i=0;i<m;i++)
+        cin>>arr[i];
+        int minn=*min_element(arr.begin(),arr.end());
+        int maxx=*max_element(arr.begin(),arr.end());
+        if(maxx-minn<=1)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
