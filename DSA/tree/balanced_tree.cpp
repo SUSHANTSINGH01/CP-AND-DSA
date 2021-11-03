27e6b8c601cc55e1b25e11aca2505761a74289f7
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-03 20:23:17 +0530

    Update balanced_tree.cpp

--- DSA/tree/balanced_tree.cpp
+++ DSA/tree/balanced_tree.cpp
@@ -26,3 +26,38 @@ class Solution{
         return ans>0?1:0;
     }
 };
+
+// second approach
+
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    int solve(Node *root,bool &ans)
+    {
+        if(root==0)
+        return 0;
+            
+        int lefft=solve(root->left,ans);
+        int righht=solve(root->right,ans);
+        
+       
+        
+        if(abs(lefft-righht)>1) 
+       {
+           ans=0;
+           return 1+max(lefft,righht);
+       }
+        
+        return 1+max(lefft,righht);
+    }
+    
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+        bool ans=1;
+        solve(root,ans);
+        
+        return ans;
+    }
+};
+
