0ccd519c27fe2a553a4247418f1b8b79610d6b90
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-04 10:56:50 +0530

    Create grps_anagrams.cpp

---
+++ DSA/string/grps_anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>res;
+        
+        
+        for(auto it: strs)
+        {
+            string tmp=it;
+            sort(tmp.begin(),tmp.end());
+            mp[tmp].push_back(it);
+        }
+        
+        for(auto it: mp)
+        res.push_back(it.second);
+        
+        return res; 
+    }
+};
