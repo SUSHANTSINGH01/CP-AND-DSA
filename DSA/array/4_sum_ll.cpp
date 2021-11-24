6dd48da5a5b84693ca88026700f8e02b61d83c99
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-24 10:00:07 +0530

    Create 4_sum_ll.cpp

---
+++ DSA/array/4_sum_ll.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        
+         int n = nums1.size();
+        unordered_map<int,int>track;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            track[nums1[i]+nums2[j]]++;            
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            ans+=track[0-nums3[i]-nums4[j]];
+        }
+        return ans;
+    }
+};
