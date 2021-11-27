ecf7c89e7ec192a9655ed1df7437b1ba64b34e98
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-27 10:38:25 +0530

    Update single_number_interview.cpp

--- DSA/bit_manipulation/single_number_interview.cpp
+++ DSA/bit_manipulation/single_number_interview.cpp
@@ -19,3 +19,13 @@ int Solution::singleNumber(const vector<int> &A)
 
         return res;
 }
+
+// second approach
+
+int Solution::singleNumber(const vector<int> &A) 
+{
+          int n=A[0];
+          for(int i=1;i<A.size();i++)
+          n=n^A[i];
+          return n;
+}
