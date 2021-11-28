23d9aa62c74e709ba14eb6a97ce6e3822f5cb903
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-11-28 13:41:32 +0530

    Update power_set.cpp

--- DSA/bit_manipulation/power_set.cpp
+++ DSA/bit_manipulation/power_set.cpp
@@ -30,3 +30,29 @@ class Solution{
 		}
 		
 };
+
+// optimal solution
+
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		   vector<string>ans;
+           int n=s.length();
+           for(int i=0;i<(1<<n);i++)
+           {
+                string res="";
+                for(int j=0;j<n;j++)
+                {
+                    if((i&(1<<j)))
+                    res+=s[j];
+                }
+                
+                if(res!="")
+                ans.push_back(res);
+            }
+           
+           sort(ans.begin(),ans.end());
+           return ans;
+		}
+};
