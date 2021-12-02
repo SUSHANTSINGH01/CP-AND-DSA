b0b5b5cf3d3ad86d66c94f963a0bd8d04b833f75
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-12-02 09:11:17 +0530

    Create arithmetic_slices.cpp

---
+++ DSA/dynamic_programming/arithmetic_slices.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) 
+    {
+        int c=0;
+        int ans=0;
+        if(nums.size()<3)
+        return 0;
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(nums[i]-nums[i+1]==nums[i+1]-nums[i+2])
+            c++;
+            else
+            c=0;
+            ans+=c;
+        }
+        return ans;
+    }
+};
