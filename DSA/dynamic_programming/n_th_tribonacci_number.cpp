4b77529bf90f36cc872e7b012c0ee7369e28e8f0
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-12-05 23:29:22 +0530

    Create n_th_tribonacci_number.cpp

---
+++ DSA/dynamic_programming/n_th_tribonacci_number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int tribonacci(int n) 
+    {
+        if(n<2)
+        return n;
+        
+        if(n==2)
+        return 1;
+        
+        int a=0;
+        int b=1;
+        int c=1;
+        int temp=0;
+        for(int i=3;i<=n;i++)
+        {
+            temp=a+b+c;
+            a=b;
+            b=c;
+            c=temp;
+        }
+        return temp;
+    }
+};
