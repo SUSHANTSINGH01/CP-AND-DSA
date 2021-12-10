6b47b067058734795ed4451332e7e5ceec0f9977
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-12-10 21:44:16 +0530

    Create find_the_highest_altitude.cpp

---
+++ DSA/prefix_sum/find_the_highest_altitude.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) 
+    {
+       int maxx=0;
+        int sum=0;
+        for(int i=0;i<gain.size();i++)
+        {
+            sum+=gain[i];
+            maxx=max(sum,maxx);
+        }
+        
+        return maxx;
+        
+        
+    }
+};
