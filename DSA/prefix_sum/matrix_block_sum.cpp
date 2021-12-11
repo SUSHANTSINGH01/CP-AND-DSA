dcb543beaf05c7242ab5e888aca0156e1abe7f33
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2021-12-11 09:48:17 +0530

    Update matrix_block_sum.cpp

--- DSA/prefix_sum/matrix_block_sum.cpp
+++ DSA/prefix_sum/matrix_block_sum.cpp
@@ -21,3 +21,34 @@ public:
         
     }
 };
+
+// optimized code using presum 
+
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) 
+    {
+        int n= mat.size();
+        int m= mat[0].size();
+        
+        vector<vector<int>> presum(n+1, vector<int> (m+1, 0));
+        vector<vector<int>>res(n, vector<int> (m, 0));
+        
+        
+        for(int i=1; i<=n; i++)
+        for(int j=1; j<=m; j++)
+        presum[i][j] = mat[i-1][j-1] + presum[i][j-1] + presum[i-1][j] - presum[i-1][j-1];
+        
+        for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+        {
+            int r1 = max(0, i-k);
+            int r2 = min(n, i+k+1);
+            int c1 = max(0, j-k);
+            int c2 = min(m, j+k+1);
+            res[i][j] = presum[r2][c2] - presum[r2][c1] - presum[r1][c2] + presum[r1][c1];
+        }
+
+        return res;
+    }
+};
