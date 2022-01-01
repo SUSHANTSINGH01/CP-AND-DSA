d4ac8264a7f4e5ff6391ca05a6cbfab9f9c1c9b6
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-01-01 15:28:16 +0530

    Create validate_stack_sequences.cpp

---
+++ DSA/stack_and_queue/validate_stack_sequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
+    {
+        stack<int>st;
+        int j=0;
+        for(int i=0; i<pushed.size(); i++) 
+        {
+            st.push(pushed[i]);
+            if(popped[j]==st.top()) 
+            {
+                while(st.size()>0 && popped[j]==st.top()) 
+                {
+                    st.pop();
+                    j++;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
