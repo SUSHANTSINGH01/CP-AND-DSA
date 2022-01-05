6f75be2bdd426b331e1c0d096b4abcecb011bb3c
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-01-05 20:15:39 +0530

    Create seat_reservation_manager.cpp

---
+++ DSA/seat_reservation_manager.cpp
@@ -0,0 +1,22 @@
+class SeatManager {
+public:
+   
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) 
+    {
+        for(int i=1; i<=n; i++)
+        pq.push(i);
+    }
+    
+    int reserve() 
+    {
+        int temp=pq.top();
+        pq.pop();
+        return temp;
+    }
+    
+    void unreserve(int seatNumber) 
+    {
+        pq.push(seatNumber);    
+    }
+};
