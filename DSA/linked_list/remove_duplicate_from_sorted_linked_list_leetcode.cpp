e17356df446f95b961b59300c02f56058388d0f2
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-01-10 23:17:16 +0530

    Update remove_duplicate_from_sorted_linked_list_leetcode.cpp

--- DSA/linked_list/remove_duplicate_from_sorted_linked_list_leetcode.cpp
+++ DSA/linked_list/remove_duplicate_from_sorted_linked_list_leetcode.cpp
@@ -1,3 +1,5 @@
+// first approach 
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) 
@@ -15,3 +17,19 @@ public:
     return head;   
     }
 };
+
+// second approach
+
+ListNode* removeDuplicates(ListNode* head) {
+	if(!head) 
+	return head;
+	if(!head->next)
+	return head;
+	
+	removeDuplicates(head->next);
+	
+	if(head->data == head->next->data)
+    head->next = head->next->next;
+	
+	return head;
+}
