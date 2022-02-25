a2d511a48774f9fd2b556c8a6d71a943639e5dd1
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-02-25 09:50:51 +0530

    Create partition_array_for_maximum_sum.cpp

---
+++ DSA/dynamic_programming/partition_array_for_maximum_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) 
+    {
+        int n=arr.size();
+        vector<int>dp(n+1);
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=arr[i-1];
+            int temp=0;
+            for(int j=1;j<=k && i-j>=0;j++)
+            {
+                temp=max(temp,arr[i-j]);
+                dp[i]=max(dp[i],temp*j+dp[i-j]);
+            }
+        }
+        return dp[n];
+    }
+};
