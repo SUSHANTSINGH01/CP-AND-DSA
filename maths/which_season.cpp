d7e95b2237335e728fc544baee10edd6ec0319a8
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-03-10 18:49:27 +0530

    Create which_season.cpp

---
+++ maths/which_season.cpp
@@ -0,0 +1,18 @@
+string Solution::solve(int A) 
+{
+   
+   if(A<1 || A>12)
+   return "Invalid";
+    
+    if(A>=3 && A<=5)
+    return "Spring";
+    
+    else if(A>=6 && A<=8)
+    return "Summer";
+
+    else if(A>=9 && A<=11)
+    return "Autumn";
+
+    else
+    return "Winter";
+}
