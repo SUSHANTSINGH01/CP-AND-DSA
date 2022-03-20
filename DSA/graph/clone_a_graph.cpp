5981810637a7fe52081f85e40242b595e555b55b
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-03-20 16:34:28 +0530

    Update clone_a_graph.cpp

--- DSA/graph/clone_a_graph.cpp
+++ DSA/graph/clone_a_graph.cpp
@@ -23,3 +23,66 @@ UndirectedGraphNode *Solution::cloneGraph(UndirectedGraphNode *node)
    }
    return mp[node];
    }
+
+
+//
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) 
+    {
+         unordered_map<int, Node*>mp; 
+         queue<Node*>q;
+         vector<bool>vis(105,false);
+         
+         if(node==0)
+         return node;
+        
+         Node* start=new Node(node->val);
+         q.push(node);
+         mp[node->val]=start;
+         vis[node->val]=true;        
+         while(!q.empty())
+         {
+            Node* cur=q.front();
+            Node* begin=mp[cur->val];
+            
+            for(auto it : cur->neighbors)
+            {
+                if(vis[it->val]==false)
+                {
+                    vis[it->val]=true;
+                    q.push(it);
+                }
+               
+                if(mp.find(it->val)==mp.end())
+                mp[it->val]=new Node(it->val);
+                
+                begin->neighbors.push_back(mp[it->val]);
+            }
+            q.pop();
+        }
+        return start;    
+    }
+};
