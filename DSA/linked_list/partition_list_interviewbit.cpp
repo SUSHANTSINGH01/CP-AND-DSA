4ac11fb93e7fea6adc721fa8038e105183bd4454
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-04-04 19:12:13 +0530

    Update partition_list_interviewbit.cpp

--- DSA/linked_list/partition_list_interviewbit.cpp
+++ DSA/linked_list/partition_list_interviewbit.cpp
@@ -43,3 +43,68 @@ ListNode* Solution::partition(ListNode* A, int B)
     }
     return start->next;
 }
+
+
+// second approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::partition(ListNode* A, int B) 
+{
+        if(A==0 || A->next==0)
+        return A;
+        
+        ListNode *head1=0;
+        ListNode *tail1=0;
+        ListNode *head2=0; 
+        ListNode *tail2=0;
+        
+        ListNode *curr=A;
+        
+        while(curr)
+        {
+            if(curr->val<B)
+            {
+                if(head1==0)
+                {
+                    head1=curr;
+                    tail1=curr;
+                }
+                else
+                {
+                    tail1->next=curr;
+                    tail1=tail1->next;
+                }
+            }
+            else
+            {
+                if(head2==0)
+                {
+                    head2=curr;
+                    tail2=curr;
+                }
+                else
+                {
+                    tail2->next=curr;
+                    tail2=tail2->next;
+                }
+            }
+            curr=curr->next;
+        }
+        
+        if(tail2)
+        tail2->next=0;
+        
+        if(head1==0)
+        return head2;
+        
+        tail1->next=head2;
+        
+        return head1;
+}
