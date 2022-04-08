ead1060b5703a7e8c4ac0a7b02595c769d53216a
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-04-09 00:22:44 +0530

    Update swapping_node_in_linked_list.cpp

--- DSA/linked_list/swapping_node_in_linked_list.cpp
+++ DSA/linked_list/swapping_node_in_linked_list.cpp
@@ -43,3 +43,76 @@ public:
     }
         
 };
+
+// optimized
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int get_len(ListNode *head)
+    {
+        int len=0;
+        while(head)
+        {
+            len++;
+            head=head->next;
+        }
+        return len;
+    }
+  
+    ListNode* swapNodes(ListNode* head, int k) 
+    {
+        int n=get_len(head);  
+		
+        if((2*k-1)==n)
+        return head;
+        
+        ListNode *x=head;
+        ListNode *x_prev=0;  
+        int cnt=k-1; 
+        
+        while(cnt--)
+        {
+            x_prev=x;
+            x=x->next;
+        }
+        
+        ListNode *y=head;
+        ListNode *y_prev=0;
+        cnt=n-k;
+        
+        while(cnt--)
+        {
+            y_prev=y;
+            y=y->next;
+        }
+    
+        if(x_prev)
+        x_prev->next=y;
+        
+        if(y_prev)
+        y_prev->next=x;
+        
+        ListNode *t=x->next;
+        x->next=y->next;
+        y->next=t;
+        
+        if(k==1)
+        return y;
+
+        if(k==n)
+        return x;
+        
+        return 
+        head;
+    }
+};
