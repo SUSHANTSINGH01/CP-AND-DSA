eedeb141971a7e2947191b40b6e966d8d51eb261
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2022-04-24 16:29:54 +0530

    Update maximum_consecutive_gap.cpp

--- DSA/array/maximum_consecutive_gap.cpp
+++ DSA/array/maximum_consecutive_gap.cpp
@@ -59,3 +59,24 @@ int Solution::maximumGap(const vector<int> &A)
     }
     return max_diff;
 }
+
+// second approach
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        
+        if(n==1)
+        return 0;
+        
+        int diff=INT_MIN;
+        
+        for(int i=0;i<n-1;i++)
+        diff=max(diff,nums[i+1]-nums[i]);
+        
+        return diff;
+    }
+};
