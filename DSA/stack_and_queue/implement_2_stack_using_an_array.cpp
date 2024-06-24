56b8aa6d56c700148d5eab82b22a246a24efb1a8
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-24 17:29:58 +0530

    Update implement_2_stack_using_an_array.cpp

--- DSA/stack_and_queue/implement_2_stack_using_an_array.cpp
+++ DSA/stack_and_queue/implement_2_stack_using_an_array.cpp
@@ -1,3 +1,110 @@
+// Optimized Solution
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class twoStacks
+{
+    public:
+
+    int top1=0;
+    int top2=201;
+    int arr[202];
+    
+    twoStacks()
+    {
+        
+    }
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        arr[top1++]=x;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+       arr[top2--]=x;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if(top1>0){
+            return arr[--top1];
+        }else{
+            return -1;
+        }
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if(top2<201) {
+            return arr[++top2];
+        }else{
+            return -1;
+        }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        twoStacks *sq = new twoStacks();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int stack_no;
+        cin>>stack_no;
+        int QueryType=0;
+        cin>>QueryType;
+        
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            if(stack_no ==1)
+            sq->push1(a);
+            else if(stack_no==2)
+            sq->push2(a);
+        }else if(QueryType==2){
+        	if(stack_no==1)
+            cout<<sq->pop1()<<" ";
+            else if(stack_no==2)
+            cout<<sq->pop2()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+// Second Solution
+
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
