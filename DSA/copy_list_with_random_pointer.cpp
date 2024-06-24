9167db855b609820130f9d43766b60c9578308e4
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-24 16:56:46 +0530

    Update and rename copy_list.cpp to copy_list_with_random_pointer.cpp

--- DSA/copy_list_with_random_pointer.cpp
+++ DSA/copy_list_with_random_pointer.cpp
@@ -1,3 +1,41 @@
+// Optimized Solution
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*,Node*>mp;
+        Node *curr=head;
+        Node *new_node=new Node(0);
+        Node *start_head=new_node;
+        while(curr!=0)
+        {
+            Node *temp=new Node(curr->val);
+            new_node->next=temp;
+            new_node=new_node->next;
+            
+            mp[curr]=new_node;
+            curr=curr->next;
+        }
+        curr=head;
+        new_node=start_head->next;
+        while(new_node!=0) 
+        {
+            new_node->random=NULL;
+            if(curr->random != NULL){
+                new_node->random=mp[curr->random];
+            }
+            new_node=new_node->next;
+            curr=curr->next;
+        }
+        return start_head->next;
+    }
+};
+
+
+// Brute Force
+
+
+
 /**
  * Definition for singly-linked list.
  * struct RandomListNode {
