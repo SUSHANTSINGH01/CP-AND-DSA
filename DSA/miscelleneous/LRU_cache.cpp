8e9f2262c16070836c90e558dd8a019420ffc1f5
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-06-27 17:47:33 +0530

    Update LRU_cache.cpp

--- DSA/miscelleneous/LRU_cache.cpp
+++ DSA/miscelleneous/LRU_cache.cpp
@@ -1,3 +1,61 @@
+// First Solution
+
+class LRUCache {
+public:
+    unordered_map<int,int>mp;
+    unordered_map<int,int>freq;
+    queue<int>q;
+    int length;
+    
+    LRUCache(int capacity) 
+    {
+       length=capacity;
+    }
+    int get(int key) {
+        if(mp.find(key)==mp.end())
+        return -1;
+        
+        freq[key]++;
+        q.push(key);
+        return mp[key];
+    }
+    
+    void put(int key, int value) 
+    {
+        if(mp.size()<length || mp.find(key)!=mp.end())
+        {
+          mp[key]=value;
+          q.push(key);
+          freq[key]++;
+          return;
+        }
+        else
+        {
+            while(1)
+            {
+                int temp=q.front();
+                q.pop();
+                freq[temp]--;
+                if(freq[temp]==0)
+                {
+                    mp.erase(temp);
+                    break;
+                }
+            }
+            mp[key]=value;
+            q.push(key);
+            freq[key]++;
+        }
+        
+    }
+};
+
+
+
+// Second Solution
+
+
+
 class LRUCache {
 public:
     unordered_map<int,int>mp;
