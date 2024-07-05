2cbaf77caace982f0f2405f054d6d54d2e7d7b54
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-05 21:17:08 +0530

    Update 5th_July_2024.cpp

--- POTD/5th_July_2024.cpp
+++ POTD/5th_July_2024.cpp
@@ -31,3 +31,49 @@ class Solution {
         return mp.size();
     }
 };
+
+
+// Java Solution
+
+
+class Solution {
+    // Function to find the vertical width of a Binary Tree.
+    class NodeLevelPair {
+        Node node;
+        int level;
+
+        NodeLevelPair(Node node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+    public int verticalWidth(Node root) {
+        // code here.
+        
+        if(root==null)
+        return 0;
+        
+        Map<Integer,Integer> mp = new HashMap<>();
+        Queue<NodeLevelPair>q = new LinkedList<>();
+        q.add(new NodeLevelPair(root,0));
+        
+        while(!q.isEmpty()){
+            
+            NodeLevelPair temp = q.poll();
+            Node node = temp.node;
+            int verticalPoint = temp.level;
+            
+            mp.put(verticalPoint,mp.getOrDefault(verticalPoint,0)+1);
+            
+            if(node.left!=null){
+                q.add(new NodeLevelPair(node.left,verticalPoint-1));
+            }
+            
+            if(node.right!=null){
+                q.add(new NodeLevelPair(node.right,verticalPoint+1));
+            }
+        }
+        
+        return mp.size();
+    }
+}
