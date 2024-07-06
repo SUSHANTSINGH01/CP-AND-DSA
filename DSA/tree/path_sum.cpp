373150f090655936a2b1cf1c4f361fb14566ed8a
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-06 15:26:22 +0530

    Update path_sum.cpp

--- DSA/tree/path_sum.cpp
+++ DSA/tree/path_sum.cpp
@@ -1,3 +1,35 @@
+
+
+// First Solution
+
+
+int solve(TreeNode *root, int b)
+{
+    
+    if (!root)
+    return 0;
+
+    if(!root->left && !root->right && root->val==b)
+    return 1;
+    
+    b-=root->val;
+    int l=solve(root->left,b);
+    cout<<b<<endl;
+    int r=solve(root->right,b);
+    
+    return l||r;
+}
+int Solution::hasPathSum(TreeNode* A, int B) 
+{
+    int sum=0;
+    int res=solve(A,B);
+    return res;
+}
+
+
+// Second Solution
+
+
  /**
  * Definition for binary tree
  * struct TreeNode {
