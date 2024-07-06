6cfba96e6f30d23b02c3040576cd79524ac1593c
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-06 14:44:40 +0530

    Update 6th_July_2024.cpp

--- POTD/6th_July_2024.cpp
+++ POTD/6th_July_2024.cpp
@@ -20,3 +20,28 @@ class Solution {
         solve(root);
     }
 };
+
+
+// Java Solution
+
+
+class Solution {
+    Node prev = null;
+    void solve(Node root){
+        if(root==null)
+        return ;
+        
+        solve(root.left);
+        
+        if(prev!=null)
+        prev.next=root;
+        
+        prev = root;
+        
+        solve(root.right);
+    }
+    public void populateNext(Node root) {
+        // code here
+        solve(root);
+    }
+}
