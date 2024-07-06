3c1f3ccd9b3d426ef2d95521a243266558536e50
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-06 15:44:08 +0530

    Update sum_tree.cpp

--- DSA/tree/sum_tree.cpp
+++ DSA/tree/sum_tree.cpp
@@ -1,3 +1,40 @@
+
+
+// First Solution
+
+
+class Solution
+{
+    public:
+    
+    int sumTree(Node *root){
+        
+        if(!root)
+        return 0;
+        
+        if(!root->left && !root->right)
+        return root->data;
+        
+        int l = sumTree(root->left);
+        int r = sumTree(root->right);
+        
+        if(l==-1 || r==-1 || (l+r)!=root->data)
+        return -1;
+        
+        else
+        return l+r+root->data;
+    }
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         return sumTree(root)==-1?0:1;
+    }
+};
+
+
+// Second Solution
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
