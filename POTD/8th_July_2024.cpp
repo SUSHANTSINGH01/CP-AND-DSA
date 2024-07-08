d87e8da6f6c59a0ffe270d3ba3edbaefd482d67d
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-08 22:37:22 +0530

    Create 8th_July_2024.cpp

---
+++ POTD/8th_July_2024.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        // complete the function here
+        int l=0;
+        int r=arr.size()-1;
+        
+        while(l<=r){
+            int mid=(r+l)/2;
+            
+            if(arr[mid]==key)
+            return mid;
+            
+            else if(arr[l]<=arr[mid]){
+                if(arr[l]<=key && key<=arr[mid])
+                r=mid-1;
+                else
+                l=mid+1;
+            }
+            
+            else {
+                if(arr[mid]<=key && key<=arr[r])
+                l=mid+1;
+                else
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
