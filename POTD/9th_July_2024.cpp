b90acd901d4fbdc2e1744cae95f2b07d3cc1559d
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-09 20:41:04 +0530

    Update 9th_July_2024.cpp

--- POTD/9th_July_2024.cpp
+++ POTD/9th_July_2024.cpp
@@ -25,3 +25,34 @@ class Solution {
         return closestSum;
     }
 };
+
+
+// Java Solution
+
+
+class Solution {
+    static int threeSumClosest(int[] array, int target) {
+        // code here
+        
+        Arrays.sort(array);
+        int closestDiff=2000000000;
+        int closestSum=2000000000;
+        for(int i=0;i<array.length-2;i++){
+            int l=i+1;
+            int r=array.length-1;
+            while(l<r){
+                int currentSum=array[i]+array[l]+array[r];
+                int currentDiff=Math.abs(currentSum-target);
+                if((currentDiff==closestDiff&&currentSum>closestSum)||currentDiff<closestDiff){
+                    closestSum=currentSum;
+                    closestDiff=currentDiff;
+                }
+                if(currentSum<target)
+                l++;
+                else
+                r--;
+            }
+        }
+        return closestSum;
+    }
+}
