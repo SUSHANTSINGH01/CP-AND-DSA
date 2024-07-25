cb750e216b4ed01058c5ffb0b634ef58886e713a
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-07-25 20:40:15 +0530

    Update flood_fill_algorithm.cpp

--- DSA/graph/flood_fill_algorithm.cpp
+++ DSA/graph/flood_fill_algorithm.cpp
@@ -1,3 +1,8 @@
+
+
+// first approach
+
+
 class Solution {
 public: 
 void solve(vector<vector<int>>& image,int x,int y,int r,int c,int nclr,int clr)
@@ -6,7 +11,7 @@ void solve(vector<vector<int>>& image,int x,int y,int r,int c,int nclr,int clr)
         if(x<0||y<0||x>r||y>c||image[x][y]!=clr)
         return;
         
-		image[x][y]=nclr;
+	image[x][y]=nclr;
         
         solve(image,x-1,y,r,c,nclr,clr);   
         solve(image,x+1,y,r,c,nclr,clr);    
@@ -24,6 +29,44 @@ void solve(vector<vector<int>>& image,int x,int y,int r,int c,int nclr,int clr)
         return image;
         
 		solve(image,sr,sc,r-1,c-1,newColor,clr);
+        return image;
+    }
+};
+
+
+// Second Approach 
+
+
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        queue<pair<int,int>>vp;
+        vp.push({sr,sc});
+
+        int start = image[sr][sc];
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m, false));
+        int xCoord[4] = {1,0,-1,0};
+        int yCoord[4] = {0,1,0,-1};
+
+        while(!vp.empty()){
+            int x=vp.front().first;
+            int y=vp.front().second;
+            vp.pop();
+            image[x][y]=color;
+            for(int i=0;i<4;i++){
+               int new_x=x+xCoord[i];
+               int new_y=y+yCoord[i];
+               if(new_x>=0 && new_y>=0 && new_x<n && new_y<m){
+                    if(image[new_x][new_y]==start && vis[new_x][new_y]==false){
+                         vp.push({new_x,new_y});
+                    }
+                    vis[new_x][new_y]=true;
+               }
+            }
+        }
         return image;
     }
 };
