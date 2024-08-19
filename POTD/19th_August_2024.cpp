2549eaeb45d02459f938d07195b2c83896614660
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2024-08-19 21:10:52 +0530

    Create 19th_August_2024.cpp

---
+++ POTD/19th_August_2024.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        
+        priority_queue<int>pq;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+           pq.push(arr[i]);
+           if(pq.size()>k)
+           pq.pop();
+        }
+        return pq.top();
+    }
+};
