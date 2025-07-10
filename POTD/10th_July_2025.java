27333f95e7bebf2d7459f4bc9a7f3398ff0499ee
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2025-07-10 23:51:41 +0530

    Create 10th_July_2025.java

---
+++ POTD/10th_July_2025.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestString(String[] words) {
+        // code here
+       
+        String res="";
+        Set<String>st=new HashSet<>();
+        
+        Arrays.sort(words); 
+        
+        for(String word:words)
+        {
+            int n=word.length();
+            if(n==1 || st.contains(word.substring(0,n-1)))
+            {
+                st.add(word);
+                if(word.length()>res.length() || word.length()==n && word.compareTo(res)<0)
+                res=word;
+                
+            }
+        }
+        
+        return res;
+    }
+}
