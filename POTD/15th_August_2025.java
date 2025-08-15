990b866a49e6a888eefa69bab21916a6c50f14cc
SUSHANTSINGH01 <62600218+SUSHANTSINGH01@users.noreply.github.com>
2025-08-15 23:47:55 +0530

    Update 15th_August_2025.java

--- POTD/15th_August_2025.java
+++ POTD/15th_August_2025.java
@@ -1,47 +1,24 @@
- int n = intervals.length;
+class Solution {
+    public ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        // code here
+        int n = intervals.length;
         ArrayList<int[]> res = new ArrayList<>();
-        int[] temp = new int[2];
-        temp[0]=intervals[0][0];
-        temp[1]=intervals[0][1];
-        int f=0;
+        int i=0;
+
+        while(i<n && newInterval[0]>intervals[i][1]) 
+        res.add(intervals[i++]);
         
-        for(int i=1;i<n;i++){
-            
-            if(f==0 && newInterval[1]<temp[0]){
-                res.add(newInterval);  
-                f=1;
-            }
-    
-           if(newInterval[0]<=temp[1] && newInterval[1]>=temp[0] && f==0){
-                temp[0]=Math.min(temp[0],newInterval[0]);
-                temp[1]=Math.max(temp[1],newInterval[1]);
-                f=1;
-            }
-            
-            if(intervals[i][0]<=temp[1]){
-                temp[0]=Math.min(temp[0],intervals[i][0]);
-                temp[1]=Math.max(temp[1],intervals[i][1]);
-            }
-            else{
-                res.add(new int[]{temp[0],temp[1]});
-                temp[0]=intervals[i][0];
-                temp[1]=Math.max(temp[1],intervals[i][1]);
-            }
-            
+
+        while(i<n && newInterval[1]>=intervals[i][0]){
+            newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1]=Math.max(newInterval[1], intervals[i++][1]);
         }
         
-       if(f==0){
-            if(temp[1]<newInterval[0]){
-                res.add(temp);
-                res.add(newInterval);
-            }else{
-                temp[0]=Math.min(temp[0],newInterval[0]);
-                temp[1]=Math.max(temp[1],newInterval[1]);
-                res.add(temp);
-            }
-            return res;
-        } 
-        
-        res.add(temp);
+        res.add(newInterval);
+
+        while(i<n)
+        res.add(intervals[i++]);
+
         return res;
     }
+}
